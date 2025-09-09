29e499cf006cc31f79dff295f2bfb1739f7fbc92
bizora21 <vijaronaa@gmail.com>
2025-09-09 21:45:39 +0200

    Update vite.config.ts
    corrigir erros

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["lovable-tagger"]
+  },
+  esbuild: {
+    loader: "ts"
   },
   build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    target: "esnext"
+  }
+});
